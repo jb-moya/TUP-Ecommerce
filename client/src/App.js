0c564d0328a69dad0cab1d8746fb68553525790c
Epiclius <jbvhert.moya@tup.edu.ph>
2024-03-15 11:11:16 +0800

    font sizes

--- client/src/App.js
+++ client/src/App.js
@@ -21,7 +21,7 @@ function App() {
                 </Routes>
             </BrowserRouter>
             {/* https://react-bootstrap.netlify.app/docs/getting-started/theming */}
-            <Button variant="primary">Primary</Button>{" "}
+            {/* <Button variant="primary">Primary</Button>{" "}
             <Button variant="secondary">Secondary</Button>{" "}
             <Button variant="success">Success</Button>{" "}
             <Button variant="warning">Warning</Button>{" "}
@@ -29,7 +29,7 @@ function App() {
             <Button variant="info">Info</Button>{" "}
             <Button variant="light">Light</Button>{" "}
             <Button variant="dark">Dark</Button>
-            <Button variant="link">Link</Button>
+            <Button variant="link">Link</Button> */}
         </div>
     );
 }
