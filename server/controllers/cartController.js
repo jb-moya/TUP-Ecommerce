43ec66cc6d86007e9b0d92c04209f4737759e75d
David Linao <davidclinao51003@gmail.com>
2024-03-16 16:00:56 +0800

    CART (updated)

--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -5,66 +5,128 @@ import { createCustomError, CustomAPIError } from "../errors/index.js";
 import { checkPermissions } from "../utils/index.js";
 import asyncWrapper from "../middleware/async.js";
 
-const addToCart = asyncWrapper(async (req, res, next) => {
-    const { orderItems } = req.body;
-    const { productId, sku, quantity } = orderItems[0];
-
-    const product = await Product.findById(productId);
-
-    if (!product) {
-        return next(
-            createCustomError(`Product with ID ${productId} not found`, 404)
-        );
-    }
-
-    let newItem;
-
-    if (sku.length === 0) {
-        newItem = {
-            name: product.name,
-            sku: "",
-            option: {},
-            price: product.price[0],
-            quantity: quantity,
-            product: product._id,
-        };
-    } else {
-        const skuSearch = product.variation.find(
-            (variation) => variation.sku === sku
-        );
-
-        if (!skuSearch) {
-            return next(
-                createCustomError(`Product ${productId} has no SKU ${sku}`, 404)
-            );
-        }
-
-        const { option, price } = skuSearch;
-        newItem = {
-            name: product.name,
-            sku: sku,
-            option: option,
-            price: price,
-            quantity: quantity,
-            product: product._id,
-        };
-    }
-
+const createCart = asyncWrapper(async (req, res, next) => {
     let cart = await Cart.findOne({ user: req.user.userId });
 
     if (!cart) {
         cart = new Cart({
             user: req.user.userId,
-            orderItems: [newItem],
+            orderItems: [],
             status: "unpaid",
             total: 0,
         });
+
+        await cart.save();
+        res.status(200).json({ cart });
     } else {
-        cart.orderItems.push(newItem);
+        res.status(400).json({ message: "A cart already exists for this user." });
+    }
+});
+
+const getSingleCart = asyncWrapper(async (req, res, next) => {
+    const {
+        params: { id: cartID },
+    } = req;
+
+    const cart = await Cart.findOne({
+        _id: cartID
+    });
+
+    if (!cart) {
+        return next(
+            createCustomError(`No cart with id : ${cartID}`, 404)
+        );
     }
 
-    await cart.save();
+    res.status(StatusCodes.OK).json({ cart });
+});
+
+const getAllCarts = asyncWrapper(async (req, res, next) => {
+    const carts = await Cart.find();
+
+    res.status(200).json({ carts });
+});
+
+const updateCart = asyncWrapper(async (req, res, next) => {
+    let cart = await Cart.findOne({ user: req.user.userId });
+
+    if (cart) {
+        
+        const { orderItems } = req.body;
+        const { productId, sku, quantity } = orderItems[0];
+
+        const product = await Product.findById(productId);
+
+        if (!product) {
+            return next(
+                createCustomError(`Product with ID ${productId} not found`, 404)
+            );
+        }
+
+        const existingItem = cart.orderItems.find(item => item.product.toString() === productId && item.sku === sku);
+
+        if (existingItem) {
+            // return res.status(400).json({ message: "Product with this SKU or productId already exists in the cart." });
+            existingItem.quantity += quantity;
+        } else {
+            let newItem;
+
+            if (sku.length === 0) {
+                newItem = {
+                    name: product.name,
+                    sku: "",
+                    option: {},
+                    price: product.price[0],
+                    quantity: quantity,
+                    product: product._id,
+                };
+            } else {
+                const skuSearch = product.variation.find(
+                    (variation) => variation.sku === sku
+                );
+
+                if (!skuSearch) {
+                    return next(
+                        createCustomError(`Product ${productId} has no SKU ${sku}`, 404)
+                    );
+                }
+
+                const { option, price } = skuSearch;
+                newItem = {
+                    name: product.name,
+                    sku: sku,
+                    option: option,
+                    price: price,
+                    quantity: quantity,
+                    product: product._id,
+                };
+            }
+
+            cart.orderItems.push(newItem);
+        }
+
+        await cart.save();
+        res.status(200).json({ cart });
+    } else {
+        res.status(400).json({ message: "No cart for this user" });
+    }
+});
+
+const deleteCart = asyncWrapper(async (req, res, next) => {
+    const { id: CartID } = req.params;
+    const cart = await Cart.findOneAndDelete({ _id: CartID });
+
+    if (!cart) {
+        return next(
+            createCustomError(`No cart with id : ${CartID}`, 404)
+        );
+    }
+
     res.status(200).json({ cart });
 });
 
-export { addToCart };
+export { createCart,
+        getSingleCart,
+        getAllCarts,
+        updateCart,
+        deleteCart };
--- server/routes/CartRoute.js
+++ server/routes/CartRoute.js
@@ -4,9 +4,22 @@ import { authorizePermissions } from "../middleware/authorization.js";
 import { authenticateUser } from "../middleware/authentication.js";
 
 import {
-    addToCart,
+    createCart,
+    getSingleCart,
+    getAllCarts,
+    updateCart,
+    deleteCart,
 } from "../controllers/cartController.js";
 
-router.route("/").post([authenticateUser, authorizePermissions('customer')], addToCart);
+router
+    .route("/")
+    .get(getAllCarts)
+    .post([authenticateUser, authorizePermissions('customer')], createCart);
+
+router
+    .route("/:id")
+    .get(authenticateUser, getSingleCart)
+    .patch([authenticateUser, authorizePermissions('customer')], updateCart)
+    .delete([authenticateUser, authorizePermissions('customer')], deleteCart);
 
 export default router;
