8ce532ff073851fe11004420092de87f6099b765
David Linao <davidclinao51003@gmail.com>
2024-05-04 00:40:21 +0800

    ACCOUNT-SETTINGS (updated)

--- server/routes/UserRoute.js
+++ server/routes/UserRoute.js
@@ -4,7 +4,7 @@ const router = express.Router();
 import {
     getAllUsers,
     showCurrentUser,
-    // updateUser,
+    updateUser,
     // updateUserPassword,
     getSingleUser,
 } from "../controllers/userController.js";
@@ -17,7 +17,7 @@ router
     .get(authenticateUser, authorizePermissions("admin"), getAllUsers);
 router.route("/showMe").get(authenticateUser, showCurrentUser);
 router.route("/getAccountDetails").get(authenticateUser, getSingleUser);
-// router.route("/updateUser").patch(authenticateUser, updateUser);
+router.route("/updateUser").patch(authenticateUser, updateUser);
 // router.route("/updateUserPassword").patch(authenticateUser, updateUserPassword);
 
 // router.route("/:id").get(authenticateUser, getSingleUser);
