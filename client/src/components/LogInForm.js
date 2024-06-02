b20fdebbc12b320a62f0bd6ad5425a38b9f21e00
Epiclius <jbvhert.moya@tup.edu.ph>
2024-06-02 14:30:37 +0800

    add revenue

--- client/src/components/LogInForm.js
+++ client/src/components/LogInForm.js
@@ -15,7 +15,7 @@ const LoadingSVG = () => {
         <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 200 200"
-            class="h-5 w-5 mr-3"
+            class="h-full w-10 mr-3"
         >
             <radialGradient
                 id="a12"
